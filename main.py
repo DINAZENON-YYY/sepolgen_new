8b3ca65c8118cbd4a232044ea099f72648cafe66
DINAZENON-YYY <82509401+DINAZENON-YYY@users.noreply.github.com>
2024-06-30 14:44:32 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -60,87 +60,6 @@ DIR = 2
 
 class PolGenerator:
     def __init__(self, filename : string):
-        """
-        # 以下应该不需要
-        self.label_dict = {}
-        self.tooltip_dict = {}
-
-        #label = xml.get_object("select_label")
-        self.label_dict["select_label"] = []
-
-        # label = xml.get_object("select_user_roles_label")
-        self.label_dict["select_user_roles_label"] = []
-
-        # label = xml.get_object("select_dir_label")
-        self.label_dict["select_dir_label"] = []
-
-        # label = xml.get_object("select_domain_admin_label")
-        self.label_dict["select_domain_admin_label"] = []
-
-        # label = xml.get_object("select_in_label")
-        self.label_dict["select_in_label"] = []
-
-        # label = xml.get_object("select_out_label")
-        self.label_dict["select_out_label"] = []
-
-        # label = xml.get_object("select_common_label")
-        self.label_dict["select_common_label"] = []
-
-        # label = xml.get_object("select_manages_label")
-        self.label_dict["select_manages_label"] = []
-
-        # label = xml.get_object("select_booleans_label")
-        self.label_dict["select_booleans_label"] = []
-
-        # tooltip文本用作辅助功能，可能不需要
-        # label = xml.get_object("existing_user_treeview")
-        self.tooltip_dict["existing_user_treeview"] = []
-
-        # label = xml.get_object("transition_treeview")
-        self.tooltip_dict["transition_treeview"] = []
-
-        # label = xml.get_object("in_tcp_all_checkbutton")
-        self.tooltip_dict["in_tcp_all_checkbutton"] = []
-
-        # label = xml.get_object("in_tcp_reserved_checkbutton")
-        self.tooltip_dict["in_tcp_reserved_checkbutton"] = []
-
-        # label = xml.get_object("in_tcp_unreserved_checkbutton")
-        self.tooltip_dict["in_tcp_unreserved_checkbutton"] = []
-
-        # label = xml.get_object("in_tcp_entry")
-        self.tooltip_dict["in_tcp_entry"] = []
-
-        # label = xml.get_object("in_udp_all_checkbutton")
-        self.tooltip_dict["in_udp_all_checkbutton"] = []
-
-        # label = xml.get_object("in_udp_reserved_checkbutton")
-        self.tooltip_dict["in_udp_reserved_checkbutton"] = []
-
-        # label = xml.get_object("in_udp_unreserved_checkbutton")
-        self.tooltip_dict["in_udp_unreserved_checkbutton"] = []
-
-        # label = xml.get_object("in_udp_entry")
-        self.tooltip_dict["in_udp_entry"] = []
-
-        # label = xml.get_object("out_tcp_entry")
-        self.tooltip_dict["out_tcp_entry"] = []
-
-        # label = xml.get_object("out_udp_entry")
-        self.tooltip_dict["out_udp_entry"] = []
-
-        # label = xml.get_object("out_tcp_all_checkbutton")
-        self.tooltip_dict["out_tcp_all_checkbutton"] = []
-
-        # label = xml.get_object("out_udp_all_checkbutton")
-        self.tooltip_dict["out_udp_all_checkbutton"] = []
-
-        # label = xml.get_object("boolean_treeview")
-        self.tooltip_dict["boolean_treeview"] = []
-
-        # label = xml.get_object("write_treeview")
-        self.tooltip_dict["write_treeview"] = []
-        """
 
         self.name = ""
         self.exec_path = ""
@@ -215,103 +134,23 @@ class PolGenerator:
         self.network_info["out_udp_info"] = [self.out_udp_all, self.out_udp_entry]
 
 
-        """
-        转换相关
-        
-        boolean 两列 name description
-        self.boolean_treeview = self.xml.get_object("boolean_treeview")
-        self.boolean_store = Gtk.ListStore(GObject.TYPE_STRING, GObject.TYPE_STRING)
-        self.boolean_treeview.set_model(self.boolean_store)
-        self.boolean_store.set_sort_column_id(0, Gtk.SortType.ASCENDING)
-        col = Gtk.TreeViewColumn(_("Name"), Gtk.CellRendererText(), text=0)
-        self.boolean_treeview.append_column(col)
-        col = Gtk.TreeViewColumn(_("Description"), Gtk.CellRendererText(), text=1)
-        self.boolean_treeview.append_column(col)
-        
-        role 一列 role
-        self.role_treeview = self.xml.get_object("role_treeview")
-        self.role_store = Gtk.ListStore(GObject.TYPE_STRING)
-        self.role_treeview.set_model(self.role_store)
-        self.role_treeview.get_selection().set_mode(Gtk.SelectionMode.MULTIPLE)
-        self.role_store.set_sort_column_id(0, Gtk.SortType.ASCENDING)
-        col = Gtk.TreeViewColumn(_("Role"), Gtk.CellRendererText(), text=0)
-        self.role_treeview.append_column(col)
-        
-        existing_user 一列 Existing_user
-        self.existing_user_treeview = self.xml.get_object("existing_user_treeview")
-        self.existing_user_store = Gtk.ListStore(GObject.TYPE_STRING)
-        self.existing_user_treeview.set_model(self.existing_user_store)
-        self.existing_user_store.set_sort_column_id(0, Gtk.SortType.ASCENDING)
-        col = Gtk.TreeViewColumn(_("Existing_User"), Gtk.CellRendererText(), text=0)
-        self.existing_user_treeview.append_column(col)
-
-        for i in self.all_roles:
-            iter = self.role_store.append()
-            self.role_store.set_value(iter, 0, i[:-2])
-
-        self.in_tcp_reserved_checkbutton = xml.get_object("in_tcp_reserved_checkbutton")
-        
-        transition_store 一列 application
-        self.transition_treeview = self.xml.get_object("transition_treeview")
-        self.transition_store = Gtk.ListStore(GObject.TYPE_STRING)
-        self.transition_treeview.set_model(self.transition_store)
-        self.transition_treeview.get_selection().set_mode(Gtk.SelectionMode.MULTIPLE)
-        self.transition_store.set_sort_column_id(0, Gtk.SortType.ASCENDING)
-        col = Gtk.TreeViewColumn(_("Application"), Gtk.CellRendererText(), text=0)
-        self.transition_treeview.append_column(col)
-        
-        user_transition_store 一列 application
-        self.user_transition_treeview = self.xml.get_object("user_transition_treeview")
-        self.user_transition_store = Gtk.ListStore(GObject.TYPE_STRING)
-        self.user_transition_treeview.set_model(self.user_transition_store)
-        self.user_transition_treeview.get_selection().set_mode(Gtk.SelectionMode.MULTIPLE)
-        self.user_transition_store.set_sort_column_id(0, Gtk.SortType.ASCENDING)
-        col = Gtk.TreeViewColumn(_("Application"), Gtk.CellRendererText(), text=0)
-        self.user_transition_treeview.append_column(col)
-
-        for i in self.all_users:
-            iter = self.user_transition_store.append()
-            self.user_transition_store.set_value(iter, 0, i[:-2])
-            iter = self.existing_user_store.append()
-            self.existing_user_store.set_value(iter, 0, i[:-2])
-        
-        admin_store 一列 application
-        self.admin_treeview = self.xml.get_object("admin_treeview")
-        self.admin_store = Gtk.ListStore(GObject.TYPE_STRING)
-        self.admin_treeview.set_model(self.admin_store)
-        self.admin_treeview.get_selection().set_mode(Gtk.SelectionMode.MULTIPLE)
-        self.admin_store.set_sort_column_id(0, Gtk.SortType.ASCENDING)
-        col = Gtk.TreeViewColumn(_("Application"), Gtk.CellRendererText(), text=0)
-        self.admin_treeview.append_column(col)
-        """
-
         self.role_store = []
         for i in self.all_roles:
-            # iter = self.role_store.append()
-            # self.role_store.set_value(iter, 0, i[:-2])
             self.role_store.append(i[:2])
 
         self.user_transition_store = []
         self.existing_user_store = []
         for i in self.all_users:
-            # iter = self.user_transition_store.append()
-            # self.user_transition_store.set_value(iter, 0, i[:-2])
             self.user_transition_store.append(i[:-2])
-            # iter = self.existing_user_store.append()
-            # self.existing_user_store.set_value(iter, 0, i[:-2])
             self.existing_user_store.append(i[:-2])
 
         self.transition_store = []
         self.admin_store = []
         try:
             for u in sepolicy.interface.get_user():
-                # iter = self.transition_store.append()
-                # self.transition_store.set_value(iter, 0, u)
                 self.transition_store.append(u)
 
             for a in sepolicy.interface.get_admin():
-                # iter = self.admin_store.append()
-                # self.admin_store.set_value(iter, 0, a)
                 self.admin_store.append(a)
         except ValueError as e:
             raise ValueError(e)
@@ -465,7 +304,6 @@ class PolGenerator:
                 if ':' in line:
                     key, value = line.split(":", 1)
                     key = key.strip().lower()
-                    # print(key)
                     value = value.strip()
                     if key == 'name':
                         self.name = value
@@ -475,9 +313,6 @@ class PolGenerator:
                         self.output_path = value
                     elif key == 'in_tcp_port':
                         self.in_tcp_entry = value
-                        #ports = value.split(",")
-                        #for port in ports:
-                        #    self.in_tcp_entry.append(port)
                     elif key == "in_tcp_all":
                         value = value.lower()
                         if value == 'n' or value == 'no':
@@ -486,9 +321,6 @@ class PolGenerator:
                             self.in_tcp_all = True
                     elif key == "out_tcp_port":
                         self.out_tcp_entry = value
-                        # ports = value.split(",")
-                        # for port in ports:
-                        #    self.out_tcp_entry.append(port)
                     elif key == "out_tcp_all":
                         value = value.lower()
                         if value == 'n' or value == 'no':
@@ -497,9 +329,6 @@ class PolGenerator:
                             self.out_tcp_all = True
                     elif key == 'in_udp_port':
                         self.in_udp_entry = value
-                        # ports = value.split(",")
-                        # for port in ports:
-                        #     self.in_udp_entry.append(port)
                     elif key == "in_udp_all":
                         value = value.lower()
                         if value == 'n' or value == 'no':
@@ -508,9 +337,6 @@ class PolGenerator:
                             self.in_udp_all = True
                     elif key == "out_udp_port":
                         self.out_udp_entry = value
-                        #ports = value.split(",")
-                        #for port in ports:
-                        #    self.out_udp_entry.append(port)
                     elif key == "out_udp_all":
                         value = value.lower()
                         if value == 'n' or value == 'no':
